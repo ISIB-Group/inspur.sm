46b34f1e9ef7bf4b9471afd722426abf9b531679
ISIB-Group <lifeng@inspur.com>
2020-07-06 09:18:29 +0800

    Modify the import module path

--- plugins/module_utils/ism.py
+++ plugins/module_utils/ism.py
@@ -1,4 +1,5 @@
-# coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 # Copyright (c), Inspur isib-group, 2020
 
 from __future__ import (absolute_import, division, print_function)
--- plugins/modules/add_user_group.py
+++ plugins/modules/add_user_group.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding:utf-8 -*-
 
 # Copyright(C) 2020 Inspur Inc. All Rights Reserved.
